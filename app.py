2e35da443f63cf22e29d5f4c025c6258c7498963
ripan-roy <ripanroy111@gmail.com>
2023-08-15 23:59:21 +0530

    added models

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import torch
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -21,6 +27,20 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
+def get_vector_store(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name = "BAAI/bge-base-en")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vector_store):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever = vector_store.as_retriever(),
+        memory = memory,
+    )
+    return conversation_chain
 def main():
     load_dotenv()
     st.set_page_config(page_title="pdfGPT", page_icon=":books:", layout="wide")
@@ -34,14 +54,22 @@ def main():
         pdf_docs = st.file_uploader("Upload your pdf file here", type="pdf", accept_multiple_files=True, key="pdf")
         if st.button("Upload"):
             with st.spinner("Uploading..."):
+                
                 #get the uploaded file
                 raw_text = get_pdf_text(pdf_docs)
                 # st.write(raw_text)
+
                 #get the text chunk from the pdf
                 text_chunks = get_text_chunks(raw_text)
                 # st.write(text_chunks)
-                #create vector store
                 
+                #create vector store
+                vector_store = get_vector_store(text_chunks)
+                # st.write(vector_store)
+
+                #create conversation chain
+                st.session_state.conversation = get_conversation_chain(vector_store)
 
 if __name__ == "__main__":
+    print(torch.cuda.is_available())
     main()
